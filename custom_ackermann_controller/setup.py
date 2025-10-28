946e39905d7faa5d4374a4859d6fa250c9ed4a21
Aditya Pachauri <adi.pachauri.444@gmail.com>
2025-10-28 09:32:05 +0530

    Update setup.py

--- custom_ackermann_controller/setup.py
+++ custom_ackermann_controller/setup.py
@@ -10,8 +10,16 @@ setup(
         ('share/ament_index/resource_index/packages',
             ['resource/' + package_name]),
         ('share/' + package_name, ['package.xml']),
-        ('share/' + package_name + '/config', ['config/robot_localization.yaml']),
-        ('share/' + package_name + '/launch', ['launch/joystick_teleop.launch.py']),
+        ('share/' + package_name + '/config', [
+            'config/robot_localization.yaml',
+            'config/laser_scan_matcher.yaml',
+            'config/smooth_velocity_controller.yaml'
+        ]),
+        ('share/' + package_name + '/launch', [
+            'launch/joystick_teleop.launch.py',
+            'launch/enhanced_odometry.launch.py',
+            'launch/smooth_teleop.launch.py'
+        ]),
     ],
     install_requires=['setuptools'],
     zip_safe=True,
@@ -24,6 +32,8 @@ setup(
         'console_scripts': [
             'ackermann_twist_controller = custom_ackermann_controller.ackermann_twist_controller:main',
             'wheel_odometry = custom_ackermann_controller.wheel_odometry:main',
+            'laser_scan_matcher = custom_ackermann_controller.laser_scan_matcher:main',
+            'smooth_velocity_controller = custom_ackermann_controller.smooth_velocity_controller:main',
         ],
     },
 )
