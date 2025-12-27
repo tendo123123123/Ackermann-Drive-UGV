f64ff72a8795653a13d2e7d4efe75013ba041b68
yenode <adi.pachauri.444@gmail.com>
2025-12-27 17:43:20 +0530

    modified:   bot_navigation/config/nav2_params.yaml modified:   bot_navigation/setup.py

--- bot_navigation/setup.py
+++ bot_navigation/setup.py
@@ -13,6 +13,7 @@ setup(
         ('share/' + package_name + '/launch', glob('launch/*.launch.py')),
         ('share/' + package_name + '/config', glob('config/*.yaml')),
         ('share/' + package_name + '/maps', glob('maps/*')),
+        ('share/' + package_name + '/behavior_trees', glob('behavior_trees/*.xml')),
     ],
     install_requires=['setuptools'],
     zip_safe=True,
